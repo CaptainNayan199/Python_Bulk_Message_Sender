ae1ce39a12ed5b4ebf73431c3d876e382ed0174a
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-10-11 16:21:34 +0545

    files updated

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 # So we are going to see the code on how we can send bulk message in telegram to any group or friends. #PS: creating this file only for educational purpose. Use this code carefully, dont harrass others.
 
+from telethon.sync
