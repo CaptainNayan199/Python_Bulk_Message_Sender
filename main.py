074860a3f68051696909de399762125550cd8bbb
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-10-11 16:38:10 +0545

    files updated

--- main.py
+++ main.py
@@ -21,4 +21,5 @@ async def main_function():
 
     for _ in range(20):
         await client.send_message(f_username, 'Hellloo from Nayan! This message is send by from python')
-        
+
+        await client.dis
